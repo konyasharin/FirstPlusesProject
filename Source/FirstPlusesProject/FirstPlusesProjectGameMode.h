12bb2aff63bbcb098388d6046faf3901761a64c2
konyasharin <kolechka.malyshev.04@gmail.com>
2024-01-28 23:55:32 +0700

    Initial commit

---
+++ Source/FirstPlusesProject/FirstPlusesProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstPlusesProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFirstPlusesProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFirstPlusesProjectGameMode();
+};
+
+
+
